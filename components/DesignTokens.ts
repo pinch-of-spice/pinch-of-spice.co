8c4141580595f9a3f050083a2bf3f9f9dafb3704
dex1t <dex1t@deg.ooo>
2022-05-06 15:54:32 +0900

    Set global styles by ussing emotion

---
+++ components/DesignTokens.ts
@@ -0,0 +1,15 @@
+const tokens = {
+  colors: {
+    ivory: "#fdf7f4",
+    white: "#ffffff",
+    orange: "#f29f05",
+    red: "#f24405",
+    azuki: "#8c0303",
+    black: "#231b1b",
+  },
+  siteWidth: {
+    pc: "1024px",
+    mobile: "480px",
+  },
+};
+export default tokens;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Global, css } from "@emotion/react";
+// @ts-ignore
+import ress from "ress";
+import tokens from "../components/DesignTokens";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
+
+const globalStyle = css`
+  ${ress}
+
+  html,
+  body {
+    font-family: -apple-system, "Helvetica Neue", "游ゴシック体", YuGothic,
+      "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic",
+      "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
+    color: ${tokens.colors.azuki};
+    background: ${tokens.colors.ivory};
+  }
+
+  h1,
+  h2 {
+    font-family: "Trykker", "Hiragino Mincho ProN", "Hiragino Mincho Pro",
+      游明朝, "Yu Mincho", YuMincho, HGS明朝E, メイリオ, Meiryo, serif;
+  }
+`;
