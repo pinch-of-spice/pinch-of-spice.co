121d28aeea93ff40cc3f4ab6d7b5825b0702fc0a
dex1t <dex1t@deg.ooo>
2022-06-01 03:14:45 +0900

    Redesign member section

--- components/Members.tsx
+++ components/Members.tsx
@@ -8,57 +8,69 @@ export default function Members() {
       <h1 id="member">Members</h1>
       <ul css={memberStyle}>
         <li>
-          <Image
-            src="/members/hayashida.png"
-            width={364}
-            height={246}
-            alt="Koichi Hayashida"
-            loading={"eager"}
-            layout="responsive"
-          />
-          <h2>林田 幸一</h2>
+          <div css={memberCaptionStyle}>
+            <Image
+              src="/members/hayashida.jpg"
+              width={200}
+              height={200}
+              alt="Koichi Hayashida"
+              loading={"eager"}
+              layout="responsive"
+            />
+            <h2>林田 幸一</h2>
+            <span>Koichi Hayashida, Product Manager</span>
+          </div>
           <p>
             クックパッド株式会社でのWEBディレクター、子会社での新規事業開発担当を経て、株式会社リクルートコミュニケーションズ(現：株式会社リクルート)で新規事業開発および国内・海外事業の戦略策定に従事。現在は家業の書店チェーンを承継。京都大学大学院修了。「RINDA」発起人。
           </p>
         </li>
         <li>
-          <Image
-            src="/members/deguchi.png"
-            width={364}
-            height={246}
-            alt="Takaya Deguchi"
-            loading={"eager"}
-            layout="responsive"
-          />
-          <h2>出口 貴也</h2>
+          <div css={memberCaptionStyle}>
+            <Image
+              src="/members/deguchi.jpg"
+              width={200}
+              height={200}
+              alt="Takaya Deguchi"
+              loading={"eager"}
+              layout="responsive"
+            />
+            <h2>出口 貴也</h2>
+            <span>Takaya Deguchi, Design Engineer</span>
+          </div>
           <p>
-            デザインエンジニア、プロダクトマネージャー。クックパッド株式会社にてデジタルプロダクトを多数立ち上げ後、SaaSスタートアップの取締役CXOを経て、現在はフリーランスとして活動。水風呂とビカクシダが好き。
+            クックパッド株式会社にてデジタルプロダクトを多数立ち上げ後、コラボレーションSaaSのプロダクト責任者、HRTechスタートアップの取締役CXOを経て、現在はフリーランスとして活動。水風呂とビカクシダが好き。
           </p>
         </li>
         <li>
-          <Image
-            src="/members/kuramitsu.png"
-            width={364}
-            height={246}
-            alt="Miwa Kuramitsu"
-            loading={"eager"}
-            layout="responsive"
-          />
-          <h2>倉光 美和</h2>
+          <div css={memberCaptionStyle}>
+            <Image
+              src="/members/kuramitsu.jpg"
+              width={200}
+              height={200}
+              alt="Miwa Kuramitsu"
+              loading={"eager"}
+              layout="responsive"
+            />
+            <h2>倉光 美和</h2>
+            <span>Miwa Kuramitsu, Creative Director</span>
+          </div>
           <p>
             デザインストラテジスト。株式会社カプコンなどで家庭用ゲームタイトルのUIデザインを複数担当したのち、クックパッド株式会社にてデザイナー統括マネージャー、デザイン戦略部の部長を経験。2022年に独立し、現在はKRAFTS&Co.合同会社代表。
           </p>
         </li>
         <li>
-          <Image
-            src="/members/sika.png"
-            width={364}
-            height={246}
-            alt="and more..."
-            loading={"eager"}
-            layout="responsive"
-          />
-          <h2>And more...</h2>
+          <div css={memberCaptionStyle}>
+            <Image
+              src="/members/shika.jpg"
+              width={200}
+              height={200}
+              alt="and more..."
+              loading={"eager"}
+              layout="responsive"
+            />
+            <h2>And more...</h2>
+            <span>Coming soon</span>
+          </div>
         </li>
       </ul>
     </section>
@@ -71,16 +83,10 @@ const memberStyle = css`
   column-gap: 32px;
   row-gap: 64px;
 
-  h2 {
-    color: ${tokens.colors.black};
-    font-family: inherit;
-    margin: 16px 0 8px;
-    letter-spacing: normal;
-  }
-
   p {
     line-height: 1.8;
     text-align: justify;
+    margin-top: 18px;
   }
 
   @media (max-width: ${tokens.siteWidth.mobile}) {
@@ -94,5 +100,22 @@ const memberStyle = css`
         width: 100%;
       }
     }
+  }
+`;
+
+const memberCaptionStyle = css`
+  width: 200px;
+  color: ${tokens.colors.azuki};
+
+  h2 {
+    font-family: inherit;
+    letter-spacing: normal;
+    margin-top: 18px;
+    font-size: 32px;
+  }
+
+  span {
+    font-size: 12px;
+    display: block;
   }
 `;
